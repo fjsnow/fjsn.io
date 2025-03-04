61239506faa8eb5d8f530bc1d22401333573f742
Freddy Snow <fjsn@protonmail.com>
2025-03-04 21:58:15 +0000

    Reorganised pages into smaller components, redid frontmatter, cleaned everything up

---
+++ src/lib/post.ts
@@ -0,0 +1,35 @@
+type MDPost = {
+    frontmatter: {
+        title: string;
+        description: string;
+        published: string;
+        tags: string[];
+        listed: boolean;
+    };
+    url: string;
+};
+
+function getOrdinalDate(date: Date): string {
+    const day = date.getDate();
+    const month = date.toLocaleString('en-GB', { month: 'long' });
+    const year = date.getFullYear();
+
+    return `${day}${getOrdinalSuffix(day)} ${month}, ${year}`;
+}
+
+function getOrdinalSuffix(day: number): string {
+    if (day > 3 && day < 21) return 'th';
+    switch (day % 10) {
+        case 1:
+            return 'st';
+        case 2:
+            return 'nd';
+        case 3:
+            return 'rd';
+        default:
+            return 'th';
+    }
+}
+
+export { getOrdinalDate };
+export type { MDPost };
---
+++ src/pages/post/test.md
@@ -0,0 +1,10 @@
+---
+layout: '@/layouts/Post.astro'
+title: 'Test Post'
+description: 'Woah..'
+published: '04-03-2025'
+tags: ['123', 'xyz']
+listed: false
+---
+
+hi
