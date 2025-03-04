b7117ce697134eb9a84782d97d08c1f45d2c214b
Freddy Snow <fjsn@protonmail.com>
2025-03-04 16:58:05 +0000

    new design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { readingTime } from './reading-time.mjs';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
+    },
+    markdown: {
+        remarkPlugins: [readingTime, remarkGfm],
+        shikiConfig: {
+            theme: 'material-theme-darker',
+        },
     },
     experimental: {
         svg: true,
