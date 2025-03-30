2d63e73f7804769036a132a98aa955cdf4daf494
Freddy Snow <git@fjsn.io>
2025-03-30 22:00:08 +0100

    remove reading time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-import { readingTime } from './plugins/reading-time.mjs';
-
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
     markdown: {
-        remarkPlugins: [readingTime],
         shikiConfig: {
             theme: 'houston',
         },
