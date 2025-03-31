ec30c934988bbd5f3c905e91611be814f2fe5037
Freddy Snow <git@fjsn.io>
2025-03-31 02:28:00 +0100

    Change glob to not recursively check files

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-    loader: glob({ pattern: '**/*.md', base: './public/posts' }),
+    loader: glob({ pattern: '*.md', base: './public/posts' }),
     schema: z.object({
         title: z.string().min(1).max(20),
         published: z.coerce.date(),
