1cd3d1ab4941d7b02b72f678165598bd2f01e7ae
Freddy Snow <git@fjsn.io>
2025-03-31 02:31:46 +0100

    Ordinal suffixes on dates

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -1,9 +1,27 @@
 function formatDate(date: Date): string {
+    const day = date.getDate();
+    const suffix = getOrdinalSuffix(day);
+
     return new Intl.DateTimeFormat('en-GB', {
-        day: 'numeric',
         month: 'long',
         year: 'numeric',
-    }).format(date);
+    })
+        .format(date)
+        .replace(/^/, `${day}${suffix} `);
+}
+
+function getOrdinalSuffix(day: number): string {
+    if (day >= 11 && day <= 13) return 'th';
+    switch (day % 10) {
+        case 1:
+            return 'st';
+        case 2:
+            return 'nd';
+        case 3:
+            return 'rd';
+        default:
+            return 'th';
+    }
 }
 
 export { formatDate };
